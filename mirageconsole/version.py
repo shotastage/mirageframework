6ba73b946f5d7b1989d6929aff76b5786b30955c
Shota Shimazu <hornet.live.mf@gmail.com>
2018-06-05 04:00:30 +0900

    revise

---
+++ mirageconsole/version.py
@@ -0,0 +1,13 @@
+"""
+Mirage Console
+version.py
+
+Created by Shota Shimazu on 2018/06/05
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the Apache License, see LICENSE for detail.
+https://github.com/shotastage/mirageframework/blob/master/LICENSE
+"""
+
+__version__ = "0.1.7"
