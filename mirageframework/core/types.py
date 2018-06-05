eb1e5f593301fa8e548a612aa5e69dc2009a8b2a
Shota Shimazu <hornet.live.mf@gmail.com>
2018-06-05 12:41:49 +0900

    add new module

---
+++ mirageframework/core/types.py
@@ -0,0 +1,18 @@
+"""
+Mirage Framework
+types.py
+
+Created by Shota Shimazu on 2018/06/05
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the Apache License, see LICENSE for detail.
+https://github.com/shotastage/mirageframework/blob/master/LICENSE
+"""
+
+from typing import NoReturn
+
+Void = NoReturn
+String = str
+Int = int
+Float = float
