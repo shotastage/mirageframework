b392b683a98541ab42eef2e7d1d10bc56d87156e
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-09 01:17:35 +0900

    add optional value

---
+++ miragecore/core/optional.py
@@ -0,0 +1,33 @@
+"""
+MIRAGE Framework
+optional.py
+
+Created by Shota Shimazu on 2018/11/09
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the Apache License, see LICENSE for detail.
+https://github.com/shotastage/mirageframework/blob/master/LICENSE
+"""
+
+class Optional():
+    
+    def __init__(self, value: any):
+        self._value: any = value
+
+    def unwrap(self) -> any:
+        
+        if self._value is not None:
+            return self._value
+        else:
+            return None
+
+    def force(self) -> any:
+        
+        if self._value is not None:
+            return self._value
+        else:
+            raise ValueError("This value contains None.\n Thus, unwrapping optional value has been failed.")
+
+    def type_name(self) -> str:
+        return type(self._value).__name__
