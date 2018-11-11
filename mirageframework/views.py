76f45eb048f898d54949905c2e3c303aea2f30c7
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-11 23:49:26 +0900

    add new view class

--- mirageframework/views.py
+++ mirageframework/views.py
@@ -5,20 +5,6 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User
 from miragecore.core.types import *
 
-
-def CREATE(request: HttpRequest, serializer_class: callable) -> Response:
-        """
-        Return a status message.
-        """
-
-        serializer = serializer_class(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 class CURDView(APIView):
     """
     View to create CRUD interface for general API resource group.
