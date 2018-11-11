514b0252bd2ffcee7a57975ac64102a3731f7a49
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-11 16:25:14 +0900

    add view

---
+++ mirageframework/views.py
@@ -0,0 +1,31 @@
+from rest_framework import status
+from rest_framework import authentication, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+
+
+
+class CURDView(APIView):
+    """
+    View to create CRUD interface for general API resource group.
+
+    * Requires token authentication.
+    * Only authenticated users are able to access this view.
+    """
+
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+    def post(self, request, format = None) -> Response:
+        """
+        Return a status message.
+        """
+        serializer: callable
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
