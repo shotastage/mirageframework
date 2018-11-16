5fff15763f73f85a3a6fbd431ef7dbff5bd39252
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-16 11:33:10 +0900

    revise

---
+++ miragecore/core/utils.py
@@ -0,0 +1,20 @@
+"""
+MIRAGE Framework
+utils.py
+
+Created by Shota Shimazu on 2018/11/12
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the Apache License, see LICENSE for detail.
+https://github.com/shotastage/mirageframework/blob/master/LICENSE
+"""
+
+import sys
+
+
+input = sys.stdin.readline
+
+
+def initialize_list(d: list):
+    return [None] * d
