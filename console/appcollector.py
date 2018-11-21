f735181de48491c8a8fe5733d2501a5a8b7b6f94
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-21 11:39:02 +0900

    update copyright doc

--- console/appcollector.py
+++ console/appcollector.py
@@ -4,7 +4,7 @@ flow.py
 
 Created by Shota Shimazu on 2018/10/25
 
-Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+Copyright (c) 2017-2018 Shota Shimazu All Rights Reserved.
 
 This software is released under the Apache License, see LICENSE for detail.
 https://github.com/shotastage/mirageframework/blob/master/LICENSE
