7fbbfc1be354814d3b0d5a1899976189976c6ba6
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-25 15:38:02 +0900

    add module name

---
+++ console/framework/flow.py
@@ -0,0 +1,16 @@
+"""
+MIRAGE Console
+flow.py
+
+Created by Shota Shimazu on 2018/11/25
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the Apache License, see LICENSE for detail.
+https://github.com/shotastage/mirageframework/blob/master/LICENSE
+"""
+
+
+def module(name: str, class_name: str) -> str:
+
+    return "have.to.import.module"
