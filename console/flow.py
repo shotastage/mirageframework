4bccf486080652c4d2b009b3ac80078831bb0b8d
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-25 15:27:58 +0900

    add flow

--- console/flow.py
+++ console/flow.py
@@ -10,23 +10,7 @@ This software is released under the Apache License, see LICENSE for detail.
 https://github.com/shotastage/mirageframework/blob/master/LICENSE
 """
 
-from abc import ABCMeta, abstractmethod
-from miragecore.core import *
 
-
-class Workflow():
-
-    def __init__(self):
-        self.workflow_will_run()
-        self.workflow_running()
-        self.workflow_did_run()
-
-
-    def workflow_will_run(self) -> Void:
-        pass    
-
-    def workflow_running(self) -> Void:
-        pass
-
-    def workflow_did_run(self) -> Void:
-        pass    
+flows = (
+    'help',
+)
