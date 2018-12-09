c7cb983e9378be27e13f43ffc68873884bfb88e4
Shota Shimazu <hornet.live.mf@gmail.com>
2018-12-09 17:45:02 +0900

    revise

--- mirageframework/jwt/views.py
+++ mirageframework/jwt/views.py
@@ -1,2 +1,18 @@
-from rest_framework import generics
+"""
+MIRAGE Framework
+views.py
 
+Created by Shota Shimazu on 2018/12/04
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the Apache License, see LICENSE for detail.
+https://github.com/shotastage/mirageframework/blob/master/LICENSE
+"""
+
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+
+class JWTokenView(generics.GenericAPIView):
+    pass
